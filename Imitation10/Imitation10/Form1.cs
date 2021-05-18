4af68be5ad724bcc5e5c810aec0afa751e4be8a8
ParadiZzzze <73051260+ParadiZzzze@users.noreply.github.com>
2021-05-18 15:36:34 +0700

    Add files via upload

---
+++ Imitation10/Imitation10/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Imitation10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Game game = new Game();
+            int[] player1_res = game.common_roll();
+            int[] player2_res = game.cheat_roll();
+            label1.Text = Convert.ToString(player1_res[0]);
+            label2.Text = Convert.ToString(player1_res[1]);
+            label3.Text = Convert.ToString(player2_res[0]);
+            label4.Text = Convert.ToString(player2_res[1]);
+            label7.Text = Convert.ToString(player1_res.Sum());
+            label8.Text = Convert.ToString(player2_res.Sum());
+            string res = "player2 wins";
+            if (player1_res.Sum()>= player2_res.Sum()) res = "player1 wins";
+            label9.Text = res;
+        }
+    }
+}
---
+++ Imitation10/Imitation10/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imitation10
+{
+    class Game
+    {
+        public double[] common_probs = { 0.2,0.2,0.2,0.2,0.2};
+        
+        public int roll()
+        {
+            Random rnd = new Random();
+            double A = rnd.NextDouble();
+            int k = 0;
+            A -= common_probs[k];
+            while (A > 0)
+            {
+                k++;
+                A -= common_probs[k];
+            }
+            return k+1;
+        }
+        public int cheat_roll(double[] probs)
+        {
+            Random rnd = new Random();
+            double A = rnd.NextDouble();
+            int k = 0;
+            A -= probs[k];
+            while (A > 0)
+            {
+                k++;
+                A -= probs[k];
+            }
+            return k+1;
+        }
+
+        public int[] cheat_roll()
+        {
+            int[] res = { 0, 0 };
+            double[] cheat_probs = { 0.1, 0.1, 0.2, 0.2, 0.4};
+            res[0] = cheat_roll(cheat_probs);
+            res[1] = cheat_roll(cheat_probs);
+            return res;
+        }
+
+        public int[] common_roll()
+        {
+            int[] res = { 0, 0 };
+            res[0] = roll();
+            res[1] = roll();
+            return res;
+        }
+    }
+}
